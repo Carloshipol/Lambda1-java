0bb3b2a9201f9089a046f5b513e3b783d5df5918
carloshipol <carlaogaruda30@gmail.com>
2024-01-11 20:59:10 -0300

    exercise-lambda1

---
+++ src/application/Progam.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Progam {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	Locale.setDefault(Locale.US);
+	
+	List<Product> list = new ArrayList<>();
+	
+	list.add(new Product("TV", 900.00));
+	list.add(new Product("Notebook", 1200.00));
+	list.add(new Product("Tablet", 450.00));
+	
+	
+	list.sort( (p1, p2) ->p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+	
+	for(Product p : list) {
+		System.out.println(p);
+	}
+
+	
+	
+	sc.close();
+	}
+
+}
